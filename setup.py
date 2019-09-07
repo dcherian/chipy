b5e487849dc750346662dc0c1e7ff227c666e270
dcherian <deepak@cherian.net>
2019-09-07 13:48:22 -0600

    make pip installable.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='chipy',
+      version='0.1',
+      description='χpod analysis',
+      url='http://github.com/dcherian/chipy',
+      author='Deepak Cherian',
+      author_email='deepak@cherian.net',
+      license='GPL3',
+      packages=['chipy'],
+      zip_safe=False)
